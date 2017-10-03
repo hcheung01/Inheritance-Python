2d55ee87dea4a70065b0dd7e2791bfdbd5fcd185
Heindrick <hcheung01@gmail.com>
2017-10-02 19:29:39 -0700

    final copy

--- inheritance.py
+++ inheritance.py
@@ -4,14 +4,25 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_toys):
         print("Child Constructor Called")
         Parent.__init__(self, last_name, eye_color)
         self.number_toys = number_toys
 
-# billy_cyrus = Parent("Cyrus", "blue")
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_toys))
+
+billy_cyrus = Parent("Cyrus", "blue")
+# billy_cyrus.show_info()
 # print(billy_cyrus.last_name)
 miley_cyrus = Child("Cyrus", "Blue", 5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_toys)
+miley_cyrus.show_info()
+# print(miley_cyrus.last_name)
+# print(miley_cyrus.number_toys)
